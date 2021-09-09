dc37003d0f627401973414d72ddcfe60f08282e4
Blake <bjlarson2_8@hotmail.com>
2021-09-09 15:58:03 -0600

    Added Brushless and led controller

---
+++ LEDController.py
@@ -0,0 +1,57 @@
+import time
+from adafruit_servokit import ServoKit
+pwm = ServoKit(channels=8)
+
+pwm.set_pwm_freq(50)
+
+# Demo using LED on of the PCA9685
+# Wire up the LED  on such that 
+#    Shortleg of LED goes to GND and
+#    Long leg goes to PWM pin 
+
+pwm.set_pwm(3,0,4095)   # Full bright
+pwm.set_pwm(4,0,4095)   # Full bright
+pwm.set_pwm(5,0,4095)   # Full bright
+time.sleep(5)
+
+pwm.set_pwm(3,1024,3072)
+pwm.set_pwm(4,1024,3072)
+pwm.set_pwm(5,1024,3072) # half bright
+time.sleep(5)
+
+pwm.set_pwm(3,0,0)  #off
+pwm.set_pwm(4,0,0)  #off
+pwm.set_pwm(5,0,0)  #off
+time.sleep(5)
+
+lowlevel = 0
+highlevel = 0
+while lowlevel < 1024:
+    pwm.set_pwm(3,lowlevel,highlevel)
+    pwm.set_pwm(4,lowlevel,highlevel)
+    pwm.set_pwm(5,lowlevel,highlevel)
+    lowlevel +=1
+    highlevel +=3
+
+while highlevel < 4095:
+    pwm.set_pwm(3,lowlevel,highlevel)
+    pwm.set_pwm(4,lowlevel,highlevel)
+    pwm.set_pwm(5,lowlevel,highlevel)
+    lowlevel +=1
+    highlevel +=1
+
+while highlevel > 3072:
+    pwm.set_pwm(3,lowlevel,highlevel)
+    pwm.set_pwm(4,lowlevel,highlevel)
+    pwm.set_pwm(5,lowlevel,highlevel)
+    lowlevel +=1
+    highlevel -=1
+
+while highlevel > 0:
+    while lowlevel > 0:
+        pwm.set_pwm(3,lowlevel,highlevel)
+        pwm.set_pwm(4,lowlevel,highlevel)
+        pwm.set_pwm(5,lowlevel,highlevel)
+        lowlevel +=1
+        highlevel -=2
+    highlevel-=1
