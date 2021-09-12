8fdfc8721e7b870d43cbc69f4418c4f5cde0b2d6
Blake <bjlarson2_8@hotmail.com>
2021-09-12 00:37:00 -0600

    update to brushless motor controller and led cont

--- BrushlessMotorController.py
+++ BrushlessMotorController.py
@@ -3,25 +3,25 @@
 # Since you are testing your motor, I hope you don't have your propeller attached to it otherwise you are in trouble my friend...?
 # This program is made by AGT @instructable.com. DO NOT REPUBLISH THIS PROGRAM... actually the program itself is harmful                                             pssst Its not, its safe.
 
-import os     #importing os library so as to communicate with the system
 import time   #importing time library to make Rpi wait because its too impatient 
-os.system ("sudo pigpiod") #Launching GPIO library
-time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
-import pigpio #importing GPIO library
+import RPi.GPIO as gpio #importing GPIO library
 
-ESC=4  #Connect the ESC in this GPIO pin 
+ESC=12  #Connect the ESC in this GPIO pin 
 
-pi = pigpio.pi()
-pi.set_servo_pulsewidth(ESC, 0) 
+gpio.setmode(gpio.BOARD)
+gpio.setup(ESC, gpio.out)
 
-max_value = 2000 #change this if your ESC's max value is different or leave it be
-min_value = 700  #change this if your ESC's min value is different or leave it be
+pwm = gpio.PWM(ESC,50)
+pwm.start(0)
+
+max_value = 10 #change this if your ESC's max value is different or leave it be
+min_value = 5  #change this if your ESC's min value is different or leave it be
 print ("For first time launch, select calibrate")
 print ("Type the exact word for the function you want")
 print ("calibrate OR manual OR control OR arm OR stop")
 
 def manual_drive(): #You will use this function to program your ESC if required
-    print ("You have selected manual option so give a value between 0 and you max value")    
+    print ("You have selected manual option so give a value between 5 and 10")    
     while True:
         inp = input()
         if inp == "stop":
@@ -34,28 +34,29 @@ def manual_drive(): #You will use this function to program your ESC if required
             arm()
             break
         else:
-            pi.set_servo_pulsewidth(ESC,inp)
+            if inp >=5 and inp <=10:
+                pwm.ChangeDutyCycle(inp)   
             
                 
 def calibrate():   #This is the auto calibration procedure of a normal ESC
-    pi.set_servo_pulsewidth(ESC, 0)
+    pwm.ChangeDutyCycle(0)
     print("Disconnect the battery and press Enter")
     inp = input()
     if inp == '':
-        pi.set_servo_pulsewidth(ESC, max_value)
+        pwm.ChangeDutyCycle(max_value)
         print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
         inp = input()
-        if inp == '':            
-            pi.set_servo_pulsewidth(ESC, min_value)
+        if inp == '':
+            pwm.ChangeDutyCycle(min_value)            
             print ("Wierd eh! Special tone")
             time.sleep(7)
             print ("Wait for it ....")
             time.sleep (5)
             print ("Im working on it, DONT WORRY JUST WAIT.....")
-            pi.set_servo_pulsewidth(ESC, 0)
+            pwm.ChangeDutyCycle(0)   
             time.sleep(2)
             print ("Arming ESC now...")
-            pi.set_servo_pulsewidth(ESC, min_value)
+            pwm.ChangeDutyCycle(min_value)   
             time.sleep(1)
             print ("See.... uhhhhh")
             control() # You can change this to any other function you want
@@ -63,23 +64,23 @@ def calibrate():   #This is the auto calibration procedure of a normal ESC
 def control(): 
     print ("I'm Starting the motor, I hope its calibrated and armed, if not restart by giving 'x'")
     time.sleep(1)
-    speed = 1500    # change your speed if you want to.... it should be between 700 - 2000
-    print ("Controls - a to decrease speed & d to increase speed OR q to decrease a lot of speed & e to increase a lot of speed")
+    speed = 7.5    # change your speed if you want to.... it should be between 5 - 10
+    print ("Controls - a to decrease speed & d to increase speed OR m to decrease a lot of speed & e to increase a lot of speed")
     while True:
-        pi.set_servo_pulsewidth(ESC, speed)
+        pwm.ChangeDutyCycle(speed)   
         inp = input()
         
-        if inp == "q":
-            speed -= 100    # decrementing the speed like hell
+        if inp == "m" & speed <= 5:
+            speed -= .1    # decrementing the speed like hell
             print ("speed = %d") % speed
         elif inp == "e":    
-            speed += 100    # incrementing the speed like hell
+            speed += .1    # incrementing the speed like hell
             print ("speed = %d") % speed
         elif inp == "d":
-            speed += 10     # incrementing the speed 
+            speed += .01     # incrementing the speed 
             print ("speed = %d") % speed
         elif inp == "a":
-            speed -= 10     # decrementing the speed
+            speed -= .01     # decrementing the speed
             print ("speed = %d") % speed
         elif inp == "stop":
             stop()          #going for the stop function
@@ -97,17 +98,17 @@ def arm(): #This is the arming procedure of an ESC
     print("Connect the battery and press Enter")
     inp = input()    
     if inp == '':
-        pi.set_servo_pulsewidth(ESC, 0)
+        pwm.ChangeDutyCycle(0)   
         time.sleep(1)
-        pi.set_servo_pulsewidth(ESC, max_value)
+        pwm.ChangeDutyCycle(max_value)   
         time.sleep(1)
-        pi.set_servo_pulsewidth(ESC, min_value)
+        pwm.ChangeDutyCycle(min_value)   
         time.sleep(1)
         control() 
         
 def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
-    pi.set_servo_pulsewidth(ESC, 0)
-    pi.stop()
+    pwm.ChangeDutyCycle(0) 
+    pwm.stop()
 
 #This is the start of the program actually, to start the function it needs to be initialized before calling... stupid python.    
 inp = input()
--- LEDController.py
+++ LEDController.py
@@ -2,6 +2,10 @@ import time
 import Adafruit_PCA9685
 pwm = Adafruit_PCA9685.PCA9685()
 
+pwm.frequency = 50
+
+
+
 pwm.set_pwm_freq(50)
 
 # Demo using LED on of the PCA9685
