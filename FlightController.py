d893f1290ce465c54cb6c317621154d21a873994
Blake <bjlarson2_8@hotmail.com>
2021-09-13 15:07:08 -0600

    updated FlightContrller

--- FlightController.py
+++ FlightController.py
@@ -1,35 +1,13 @@
 import socket
 import RPi.GPIO as GPIO
-from adafruit_servokit import ServoKit
 import time
+from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels = 8)
 
-servo_min = 0
-servo_Max = 180
-
-in1 = 18
-in2 = 16
-en = 22
-temp1 = 1
-
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
-GPIO.output(in1, GPIO.LOW)
-GPIO.output(in2, GPIO.LOW)
-p = GPIO.PWM(en, 1000)
-
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in4, GPIO.OUT)
-GPIO.setup(enb, GPIO.OUT)
-GPIO.output(in3, GPIO.LOW)
-GPIO.output(in4, GPIO.LOW)
-pb = GPIO.PWM(enb, 1000)
-
-p.start(25)
+elevator = 1
+rudder = 0
+motor = 2
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("192.168.1.13",1234))
@@ -52,77 +30,25 @@ while True:
             clientsocket.send(bytes("servo command", "utf-8"))
 
             turnAngle = int(incomeing[3:len(incomeing)])
-            kit.servo[0].angle = turnAngle
+            kit.servo[rudder].angle = turnAngle
             
         elif(incomeing[0:3] == "elv"):
             print("servo command")
             clientsocket.send(bytes("servo command", "utf-8"))
 
             elevatorAngle = int(incomeing[3:len(incomeing)])
-            kit.servo[1].angle = elevatorAngle
+            kit.servo[elevator].angle = elevatorAngle
             
         elif(incomeing[0:3] == "mtr"):
             print("Motor command")
             clientsocket.send(bytes("Motor command", "utf-8"))
             
             x = incomeing[3:len(incomeing)]
-    
-            if x== 'run':
-                print("Run")
-                clientsocket.send(bytes("Run", "utf-8"))
-                
-                if(temp1 == 1):
-                    GPIO.output(in1,False)
-                    GPIO.output(in2,True)
-                    print("Forward")
-                    clientsocket.send(bytes("Forward", "utf-8"))
-                    x = 'z'
-                else:
-                    GPIO.output(in1,True)
-                    GPIO.output(in2,False)
-                    print("Backward")
-                    clientsocket.send(bytes("Backward", "utf-8"))
-                    x = 'z'
             
-            elif x== 'stp':
+            if x== 'stp':
                 print("Stop")
                 clientsocket.send(bytes("Stop", "utf-8"))
-                GPIO.output(in1,False)
-                GPIO.output(in2,False)
-                x = 'z'
-            
-            elif x== 'for':
-                print("Forward")
-                clientsocket.send(bytes("Forward", "utf-8"))
-                GPIO.output(in1,True)
-                GPIO.output(in2,False)
-                temp1 = 1
-                x = 'z'
-        
-            elif x== 'bac':
-                print("Backward")
-                clientsocket.send(bytes("Backward", "utf-8"))
-                GPIO.output(in1,False)
-                GPIO.output(in2,True)
-                temp1 = 0
-                x = 'z'
-        
-            elif x== 'low':
-                print("Low")
-                clientsocket.send(bytes("Low", "utf-8"))
-                p.ChangeDutyCycle(15)
-                x = 'z'
-        
-            elif x== 'med':
-                print("Medium")
-                clientsocket.send(bytes("Medium", "utf-8"))
-                p.ChangeDutyCycle(25)
-                x = 'z'
-        
-            elif x== 'hig':
-                print("High")
-                clientsocket.send(bytes("High", "utf-8"))
-                p.ChangeDutyCycle(50)
+                kit.servo[motor].angle = 0
                 x = 'z'
     
             elif x== 'ext':
@@ -135,7 +61,7 @@ while True:
                     motor_val = int(x)
                     print("User assigned speed")
                     clientsocket.send(bytes("User assigned speed", "utf-8"))
-                    p.ChangeDutyCycle(motor_val)
+                    kit.servo[motor].angle = motor_val
                     x = 'z'
         
                 except ValueError:
